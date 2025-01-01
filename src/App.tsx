af32c375e5b6517184050d72698a2e8e3630e231
Jehr_ <jehrman27@gmail.com>
2025-01-01 14:40:13 -0500

    add Calculator component with button functionality and Textarea; implement click handler for button actions

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import Calculator from "./components/Calculator";
 import "./App.css";
 import { ModeToggle } from "./components/mode-toggle";
 
 function App() {
   return (
     <>
-      <section>Calculator</section>
+      <section>
+        <Calculator />
+      </section>
       <aside className="fixed top-0 right-0 p-4">
         <ModeToggle />
       </aside>
--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,7 +1,26 @@
+import { Button } from "./ui/button";
 import { Card } from "./ui/card";
+import { Textarea } from "./ui/textarea";
+
+const clickHandler = (e) => {
+  console.log(e.target.innerText);
+};
 
 const Calculator = () => {
-  return <Card></Card>;
+  return (
+    <Card>
+      <Textarea className="resize-none">0</Textarea>
+      <div className="grid grid-cols-4 gap-1">
+        {[1, 2, 3, "/", 4, 5, 6, "*", 7, 8, 9, "-", "C", 0, "=", "+"].map(
+          (key) => (
+            <Button key={key} onClick={clickHandler}>
+              {key}
+            </Button>
+          )
+        )}
+      </div>
+    </Card>
+  );
 };
 
 export default Calculator;
